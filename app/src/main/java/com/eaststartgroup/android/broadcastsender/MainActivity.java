7a82d8f250bb661a1a775626425f68a270d82613
Tom, TANG Nan Zeng <nztang68@hotmail.com>
2017-10-03 22:42:12 +0800

    Initial commit

---
+++ app/src/main/java/com/eaststartgroup/android/broadcastsender/MainActivity.java
@@ -0,0 +1,28 @@
+package com.eaststartgroup.android.broadcastsender;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        final Button button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction("com.eaststartgroup.android.NEW_BROADCAST");
+                intent.putExtra("broadcast","Hello!");
+                sendBroadcast(intent);
+            }
+        });
+    }
+
+}
